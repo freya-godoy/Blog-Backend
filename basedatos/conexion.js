343a144bf8f21ab1f5c3dfbf9acfa87ae13f3502
freya-godoy <freya.osorio.20@gmail.com>
2025-01-30 20:31:39 -0300

    agregue readme

--- basedatos/conexion.js
+++ basedatos/conexion.js
@@ -4,7 +4,7 @@ const conexion = async() => {
 
     try {
 
-        await mongoose.connect("mongodb://localhost:27017/mi_blog");
+        await mongoose.connect("mongodb://127.0.0.1:27017/mi_redsocial");
 
         // Parametros dentro de objeto // solo en caso de aviso
         // useNewUrlParser: true
